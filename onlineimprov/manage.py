741f94b49e5ce21f9f1b7818749a2ff78b95ff28
Chris <clj@ollure.com>
2020-11-26 18:32:45 -0600

    manage py

--- onlineimprov/manage.py
+++ onlineimprov/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onlineimprov.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onlineimprov.onlineimprov.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
